db858f10f722c2b1723f6f9b0c9095d4b2634a11
piccolaMaja <todorovic.maja@hotmail.com>
2018-09-25 22:13:13 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.android.inventory;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.android.inventory.data.OrdersContract;
+import com.example.android.inventory.data.OrdersDbHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.insert_new_task:
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
+                return true;
+
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete_all_entries:
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
